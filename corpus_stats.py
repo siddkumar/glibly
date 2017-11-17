24f5ebcb2a3a1d1e68b86cf03c22c78965864c05
qmac <quinten.mcnamara@gmail.com>
2017-11-17 00:05:38 -0600

    updated corpus stats for new class labels

--- corpus_stats.py
+++ corpus_stats.py
@@ -1,21 +1,19 @@
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-
     counter = {}
     with open('adverbs.txt', 'r') as adverbs:
         for line in adverbs:
             word = line.split()[0]
-            cleanword = word[1:-1]
 
-            if cleanword in counter:
-                counter[cleanword] = counter[cleanword] + 1
+            if word in counter:
+                counter[word] = counter[word] + 1
             else:
-                counter[cleanword] = 1
+                counter[word] = 1
 
     num_w = 0
     total = 0
-    for w in sorted( counter.keys()):
+    for w in sorted(counter.keys()):
         num_w += 1
         total += counter[w]
         # print w, counter[w]
