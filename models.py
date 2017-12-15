007a98d01093523583ac68160b1a25a0daf9c9f6
qmac <quinten.mcnamara@gmail.com>
2017-12-15 17:58:33 -0600

    updated models to compatible with run.py

--- models.py
+++ models.py
@@ -1,66 +1,36 @@
 from __future__ import division
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import cross_val_score
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix, accuracy_score
 
-from corpus_stats import get_counter
-import re
-import string
-
-vectorizer = CountVectorizer(min_df=2, analyzer='char', ngram_range=(2,3))
+vectorizer = CountVectorizer(min_df=2, analyzer='char', ngram_range=(2, 3))
 # vectorizer = CountVectorizer(min_df=2, analyzer='word', ngram_range=(1,1))
 
-labels = []
-examples = []
 
-filename = 'adverbs.txt'
-with open(filename) as input_file:
-    counter = get_counter(filename)
-    data = input_file.readlines()
+def svm(train_examples, train_labels, test_examples, test_labels, num_labels, embedding_dim):
+    training_matrix = vectorizer.fit_transform(train_examples)
+    test_matrix = vectorizer.transform(test_examples)
 
+    clf = SVC(kernel='linear', probability=True)
+    clf.fit(training_matrix, train_labels)
+    preds = clf.predict(test_matrix)
+    probs = clf.predict_proba(test_matrix)
+    print confusion_matrix(preds, test_labels)
+    print accuracy_score(preds, test_labels)
 
-clusters = {}
-for i, l in enumerate(open('clusters.txt')):
-    for w in l.split():
-        clusters[w] = i
+    correct_r_3 = 0.0
+    correct_r_5 = 0.0
+    for ex_idx in xrange(len(test_examples)):
+        lps = [(i, probs[ex_idx][i]) for i in xrange(len(probs[ex_idx]))]
+        slps = sorted(lps, key=lambda tup: tup[1], reverse=True)
+        top_3 = [slps[t][0] for t in range(3)]
+        top_5 = [slps[t][0] for t in range(5)]
 
-for item in data:
-    label, sentence = item.split('\t')
-    if not label in counter or counter[label] < 10 or label not in clusters:
-        continue
+        if test_labels[ex_idx] in top_3:
+            correct_r_3 += 1.0
+            correct_r_5 += 1.0
+        elif test_labels[ex_idx] in top_5:
+            correct_r_5 += 1.0
 
-    orig_sentence = sentence.decode('utf-8')
-    sentence = orig_sentence.lower()
-    sentence = re.sub('[' + string.punctuation + ']' , '', sentence)
-
-    stylometric_features = []
-    scale = float(len(orig_sentence))
-    for c in string.punctuation:
-        stylometric_features.append(orig_sentence.count(c) / scale)
-
-    labels.append(clusters[label])
-    examples.append(stylometric_features)
-
-print "Number of examples: ", len(examples)
-# training_matrix = vectorizer.fit_transform(examples)
-training_matrix = examples
-print "Dilily Dilly"
-
-clf = LinearSVC()
-cv_score = cross_val_score(clf, training_matrix, labels, cv=5).mean()
-
-
-test_set_size = 1000
-clf.fit(training_matrix[test_set_size:], labels[test_set_size:])
-preds =  clf.predict(training_matrix[:test_set_size])
-print confusion_matrix(preds, labels[:test_set_size])
-print accuracy_score(preds, labels[:test_set_size])
-
-count = [0] * 19
-for i, v in clusters.items():
-    count[v] += counter[i]
-print count
-print max(count) / sum(count)
-
-print("Cross validation score: {}".format(cv_score))
+    print "Dev Accuracy, Recall 3: " + str(correct_r_3/len(test_labels))
+    print "Dev Accuracy, Recall 5: " + str(correct_r_5/len(test_labels))
